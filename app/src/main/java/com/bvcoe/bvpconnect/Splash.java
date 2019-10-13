15fd18f51425f3b3bdaad756dc7a07ce3e5d17df
saksham0804 <saksham.0804@gmail.com>
2019-10-13 10:21:19 +0530

    Initial commit

---
+++ app/src/main/java/com/bvcoe/bvpconnect/Splash.java
@@ -0,0 +1,42 @@
+package com.bvcoe.bvpconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    private static int splash_screen=2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(Splash.this,
+                        MainActivity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, splash_screen);
+
+
+    }
+}
