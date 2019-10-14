26ecd1933efd858eaedc28c46edb06152c2b2d63
saksham0804 <saksham.0804@gmail.com>
2019-10-14 21:09:34 +0530

    Sign Up activity

---
+++ app/src/main/java/com/bvcoe/bvpconnect/SignUp.java
@@ -0,0 +1,14 @@
+package com.bvcoe.bvpconnect;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
