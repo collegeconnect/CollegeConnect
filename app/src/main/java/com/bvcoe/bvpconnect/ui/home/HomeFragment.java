502e92f3326012355efd33ee2bc52c63a2a952db
SAJAL JAIN <sjain30sept@gmail.com>
2019-11-13 21:27:03 +0530

    Created Home Page

--- app/src/main/java/com/bvcoe/bvpconnect/ui/home/HomeFragment.java
+++ app/src/main/java/com/bvcoe/bvpconnect/ui/home/HomeFragment.java
@@ -23,13 +23,13 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        /*final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
