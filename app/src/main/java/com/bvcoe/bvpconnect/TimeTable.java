802bdbf08b4e50e3f73ad3c5a75a859668aaebfa
SAJAL JAIN <sjain30sept@gmail.com>
2019-12-29 23:42:58 +0530

    Added upload time table feature

--- app/src/main/java/com/bvcoe/bvpconnect/TimeTable.java
+++ app/src/main/java/com/bvcoe/bvpconnect/TimeTable.java
@@ -2,10 +2,22 @@ package com.bvcoe.bvpconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class TimeTable extends AppCompatActivity {
+
+    private static final int GET_FROM_GALLERY = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +26,40 @@ public class TimeTable extends AppCompatActivity {
 
         ImageView imageView = findViewById(R.id.imageView2);
         imageView.setBackgroundResource(R.drawable.timetable);
+    }
+
+    Bitmap bitmap;
+
+    public void uploaddp(View view)
+    {
+        Intent intent =new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, GET_FROM_GALLERY);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+        //Detects request codes
+        if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                ImageView imageView =findViewById(R.id.imageView2);
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+
     }
 }
