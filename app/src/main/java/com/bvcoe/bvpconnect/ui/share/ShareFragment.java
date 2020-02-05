efcbde589edb869568138f760d244710d57a94d4
SAJAL JAIN <sjain30sept@gmail.com>
2020-02-05 21:40:33 +0530

    Enabled JavaScript

--- app/src/main/java/com/bvcoe/bvpconnect/ui/share/ShareFragment.java
+++ app/src/main/java/com/bvcoe/bvpconnect/ui/share/ShareFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -28,7 +29,8 @@ public class ShareFragment extends Fragment {
         webView = view.findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://rooms.dscbvp.dev/");
-
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         return view;
 
     }
@@ -39,15 +41,17 @@ public class ShareFragment extends Fragment {
 //        ((navigation)getActivity()).setTitle("Room Locator");
 //
 //    }
-@Override
-public void onResume() {
-    super.onResume();
-    ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
-}
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
     @Override
     public void onStop() {
         super.onStop();
         ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
+
 
 }
