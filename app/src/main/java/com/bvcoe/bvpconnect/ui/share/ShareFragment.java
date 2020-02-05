3ff7c722770ee134d1a7bb473ad71e40a1c0dcfd
saksham0804 <saksham.0804@gmail.com>
2020-02-05 21:43:18 +0530

    Merge branch 'master' of F:\Android\StudioProjects\BvpConnect with conflicts.

--- app/src/main/java/com/bvcoe/bvpconnect/ui/share/ShareFragment.java
+++ app/src/main/java/com/bvcoe/bvpconnect/ui/share/ShareFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ import com.bvcoe.bvpconnect.navigation;
 public class ShareFragment extends Fragment {
 
     WebView webView;
+    WebSettings webSettings;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -28,7 +30,8 @@ public class ShareFragment extends Fragment {
         webView = view.findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://rooms.dscbvp.dev/");
-
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         return view;
 
     }
@@ -39,15 +42,17 @@ public class ShareFragment extends Fragment {
 //        ((navigation)getActivity()).setTitle("Room Locator");
 //
 //    }
-@Override
-public void onResume() {
-    super.onResume();
-    ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
-}
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
     @Override
     public void onStop() {
         super.onStop();
         ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
+
 
 }
