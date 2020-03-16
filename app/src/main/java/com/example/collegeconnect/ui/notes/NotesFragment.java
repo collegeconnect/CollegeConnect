d36715c52cb3112ee046d3eb8f6f84befed53c50
SAJAL JAIN <sjain30sept@gmail.com>
2020-03-16 23:51:07 +0530

    Updated Notes Layout#2

--- app/src/main/java/com/example/collegeconnect/ui/notes/NotesFragment.java
+++ app/src/main/java/com/example/collegeconnect/ui/notes/NotesFragment.java
@@ -1,6 +1,5 @@
 package com.example.collegeconnect.ui.notes;
 
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -8,22 +7,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
-import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import com.example.collegeconnect.R;
 import com.example.collegeconnect.UploadNotes;
-import com.example.collegeconnect.navigation;
 import com.example.collegeconnect.ui.Download.Download;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class NotesFragment extends Fragment {
     BottomNavigationView bottomNavigationView;
     TextView tv;
-    Button viewnotes;
 
     @Nullable
     @Override
