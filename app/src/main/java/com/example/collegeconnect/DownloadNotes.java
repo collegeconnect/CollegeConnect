4a662fc61bfff8911dc563aacacb66cb532ea6aa
SAJAL JAIN <sjain30sept@gmail.com>
2020-03-17 13:51:08 +0530

    Code Clean

--- app/src/main/java/com/example/collegeconnect/DownloadNotes.java
+++ app/src/main/java/com/example/collegeconnect/DownloadNotes.java
@@ -3,21 +3,15 @@ package com.example.collegeconnect;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class DownloadNotes extends AppCompatActivity {
 
@@ -28,7 +22,6 @@ public class DownloadNotes extends AppCompatActivity {
     public static ArrayList<Upload> uploadList;
     static DatabaseReference mDatabaseReference;
     RecyclerView recyclerView;
-    ListView listView;
     NotesAdapter notesAdapter;
 
     @Override
@@ -43,7 +36,6 @@ public class DownloadNotes extends AppCompatActivity {
         String receivedUnit = intent.getStringExtra(EXTRA_UNIT);
 
         Toast.makeText(this, receivedSemester, Toast.LENGTH_SHORT).show();
-//        listView = findViewById(R.id.list);
 
         uploadList = new ArrayList<>();
         mDatabaseReference = FirebaseDatabase.getInstance().getReference(Constants.DATABASE_PATH_UPLOADS);
@@ -57,16 +49,6 @@ public class DownloadNotes extends AppCompatActivity {
                         Toast.makeText(DownloadNotes.this, upload.getName(), Toast.LENGTH_SHORT).show();
                     }
                 }
-
-//                String[] uploads = new String[uploadList.size()];
-//
-//                for (int i = 0; i < uploads.length; i++) {
-//                    uploads[i] = uploadList.get(i).getName();
-//                }
-//
-//                //displaying it to list
-//                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, uploads);
-//                listView.setAdapter(adapter);
 
                 recyclerView = findViewById(R.id.downloadRecycler);
                 recyclerView.setHasFixedSize(true);
