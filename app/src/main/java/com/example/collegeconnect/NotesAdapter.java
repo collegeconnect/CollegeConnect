d94eec5956ba1d897f252ac2e7c9a806a03eccf1
SAJAL JAIN <sjain30sept@gmail.com>
2020-04-12 22:47:24 +0530

    Added report notes button

--- app/src/main/java/com/example/collegeconnect/NotesAdapter.java
+++ app/src/main/java/com/example/collegeconnect/NotesAdapter.java
@@ -4,11 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.collegeconnect.ui.attendance.AttendanceFragment;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
@@ -57,6 +66,26 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
                 context.startActivity(Intent.createChooser(intent, "Choose an Application:"));
             }
         });
+
+        holder.report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final PopupMenu popup = new PopupMenu(context,v);
+                MenuInflater inflater = popup.getMenuInflater();
+                inflater.inflate(R.menu.report, popup.getMenu());
+                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem menuItem) {
+                        switch (menuItem.getItemId()){
+                            case R.id.report:
+                                Toast.makeText(context, "Report Notes", Toast.LENGTH_SHORT).show();
+                        }
+                        return true;
+                    }
+                });
+                popup.show();
+            }
+        });
     }
 
     @Override
@@ -67,14 +96,16 @@ public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.ViewHolder> 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView title,author,noOfDown;
-        View itv;
+        ImageButton report;
+        RelativeLayout itv;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.title);
             author = itemView.findViewById(R.id.authorname);
+            report = itemView.findViewById(R.id.reportButton);
             noOfDown = itemView.findViewById(R.id.download);
-            itv=itemView;
+            itv = itemView.findViewById(R.id.relate);
         }
     }
 }
