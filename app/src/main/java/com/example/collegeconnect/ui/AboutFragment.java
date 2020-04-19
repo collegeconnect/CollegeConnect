914c49a3cb8fd39f0ae2a7d8d9e7e31512675b8b
SAJAL JAIN <sjain30sept@gmail.com>
2020-04-19 22:23:51 +0530

    About page created

--- app/src/main/java/com/example/collegeconnect/ui/AboutFragment.java
+++ app/src/main/java/com/example/collegeconnect/ui/AboutFragment.java
@@ -7,12 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.collegeconnect.R;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class AboutFragment extends Fragment {
     TextView tv;
+    private StringBuilder text = new StringBuilder();
 
     public AboutFragment() {
         // Required empty public constructor
@@ -25,6 +32,35 @@ public class AboutFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_about, container, false);
         tv = getActivity().findViewById(R.id.settingTitle);
+
+
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getActivity().getAssets().open("about_text.txt")));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                text.append(mLine);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            Toast.makeText(getActivity(), "Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+
+            EditText output = view.findViewById(R.id.about_text);
+            output.setText((CharSequence) text);
+        }
         return view;
     }
     @Override
