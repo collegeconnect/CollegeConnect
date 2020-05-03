1e5cd69b2e5e31acb337f9c245623e527fe53c02
saksham0804 <saksham.0804@gmail.com>
2020-05-04 00:01:38 +0530

    Create event restriction

--- app/src/main/java/com/example/collegeconnect/ui/UpcomingEvents.java
+++ app/src/main/java/com/example/collegeconnect/ui/UpcomingEvents.java
@@ -14,16 +14,32 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import com.bumptech.glide.signature.ObjectKey;
 import com.example.collegeconnect.R;
+import com.example.collegeconnect.datamodels.SaveSharedPreference;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 public class UpcomingEvents extends AppCompatActivity {
     private FloatingActionButton createEvent;
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     Fragment upcomingevents = new EventsFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        databaseReference = firebaseDatabase.getReference("EventAdmin");
         setContentView(R.layout.activity_upcoming_event);
         Toolbar toolbar = findViewById(R.id.toolbarcom);
         setSupportActionBar(toolbar);
@@ -33,6 +49,20 @@ public class UpcomingEvents extends AppCompatActivity {
         TextView tv = findViewById(R.id.tvtitle);
         tv.setText("Upcoming Events");
         createEvent = findViewById(R.id.createEvent);
+        createEvent.setVisibility(View.GONE);
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<String> arrayList = (ArrayList<String>) dataSnapshot.getValue();
+                if(arrayList.contains(SaveSharedPreference.getUserName(UpcomingEvents.this)))
+                    createEvent.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         createEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
