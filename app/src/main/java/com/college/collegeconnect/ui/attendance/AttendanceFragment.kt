aea9fcd6166d4492b77e4daf7cecc4f5f340b829
= <sjain30sept@gmail.com>
2020-09-27 23:46:54 +0530

    New UI #3

--- app/src/main/java/com/college/collegeconnect/ui/attendance/AttendanceFragment.kt
+++ app/src/main/java/com/college/collegeconnect/ui/attendance/AttendanceFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,7 +13,6 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -21,17 +21,15 @@ import com.college.collegeconnect.R
 import com.college.collegeconnect.adapters.SubjectAdapter
 import com.college.collegeconnect.database.AttendanceDatabase
 import com.college.collegeconnect.database.SubjectDetails
-//import com.college.collegeconnect.datamodels.DatabaseHelper
+import com.college.collegeconnect.datamodels.SaveSharedPreference
 import com.college.collegeconnect.models.AttendanceViewModel
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.fragment_attendance.*
 import java.util.*
 
 class AttendanceFragment : Fragment() {
     lateinit var bottomNavigationView: BottomNavigationView
-//    lateinit var mydb: DatabaseHelper
     lateinit var subject: TextInputLayout
     lateinit var addSubject: Button
     private lateinit var subjectRecycler: RecyclerView
@@ -39,13 +37,12 @@ class AttendanceFragment : Fragment() {
     lateinit var mCtx: Context
     lateinit var subjectList: ArrayList<SubjectDetails?>
     private lateinit var viewModel: AttendanceViewModel
-
+    private var criteria = 0f
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_attendance, container, false)
         subjectRecycler = view.findViewById(R.id.subjectRecyclerView)
         subject = view.findViewById(R.id.subjectNamemas)
-        addSubject = view.findViewById(R.id.addSubject)
         subject.editText?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
@@ -67,13 +64,70 @@ class AttendanceFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         tv = requireActivity().findViewById(R.id.navTitle)
         tv.text = "ATTENDANCE"
+
+        //Set target attendance criteria
+        criteria = SaveSharedPreference.getAttendanceCriteria(context).toFloat()
+        Log.d("TAG", "criteria: $criteria")
+        att_dp.apply {
+//            progress = criteria
+            setProgressWithAnimation(criteria, 1000) // =1s
+        }
+
         if (activity != null) bottomNavigationView = requireActivity().findViewById(R.id.bottomNav)
-//        mydb = DatabaseHelper(context)
         subjectList = ArrayList()
         subjectRecycler.setHasFixedSize(true)
         subjectRecycler.layoutManager = LinearLayoutManager(context)
         load()
-        addSubject.setOnClickListener { addSubject() }
+
+        //Add subject by pressing enter on keyboard
+        subjectNamemas.editText!!.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                if (event.getAction() === KeyEvent.ACTION_DOWN) {
+                    when (keyCode) {
+                        KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                            addSubject()
+                            return true
+                        }
+                        else -> {
+                        }
+                    }
+                }
+                return false
+            }
+        })
+
+        //Calculate aggregate attendance
+        val attended = AttendanceDatabase(requireContext()).getAttendanceDao().getAttended()
+        attended.observe(requireActivity(), Observer { atten ->
+            AttendanceDatabase(requireContext()).getAttendanceDao().getMissed().observe(requireActivity(), Observer { miss ->
+                if (atten != null && miss != null) {
+                    val percentage = atten.toFloat() / (atten.toFloat() + miss.toFloat())
+                    if (!percentage.isNaN()) {
+                        att_dp2.apply {
+//                            progress = percentage
+                            setProgressWithAnimation(65f, 1500) // =1s
+                        }
+                        Log.d("TAG", "attended: $percentage")
+                    }
+                }
+            })
+        })
+        val missed = AttendanceDatabase(requireContext()).getAttendanceDao().getMissed()
+        missed.observe(requireActivity(), Observer { miss ->
+            AttendanceDatabase(requireContext()).getAttendanceDao().getAttended().observe(requireActivity(), Observer { atten ->
+                if (atten != null && miss != null) {
+                    val percentage = atten.toFloat().div((atten.toFloat() + miss.toFloat()))
+                    if (!percentage.isNaN()) {
+                        att_dp2.apply {
+//                            progress = percentage
+                            setProgressWithAnimation(65f, 1500) // =1s
+                        }
+                        Log.d("TAG", "missed: $percentage")
+                    }
+                }
+            })
+        })
+
     }
 
 //    private fun loadData() {
@@ -172,11 +226,6 @@ class AttendanceFragment : Fragment() {
         super.onResume()
         bottomNavigationView.menu.findItem(R.id.nav_attendance).isChecked = true
     }
-
-//    override fun onDestroy() {
-//        super.onDestroy()
-//        job.cancel()
-//    }
 
     companion object {
         lateinit var subjectAdapter: SubjectAdapter
