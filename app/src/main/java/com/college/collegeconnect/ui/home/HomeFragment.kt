9ff441e1fe8e47400802b110c020583b390ffdc6
Sajal Jain <sjain30sept@gmail.com>
2021-10-12 20:32:36 +0530

    ViewBinding in HomeFragment.kt

--- app/src/main/java/com/college/collegeconnect/ui/home/HomeFragment.kt
+++ app/src/main/java/com/college/collegeconnect/ui/home/HomeFragment.kt
@@ -20,6 +20,7 @@ import com.amulyakhare.textdrawable.TextDrawable
 import com.college.collegeconnect.BuildConfig
 import com.college.collegeconnect.R
 import com.college.collegeconnect.adapters.HomeRecyclerAdapter
+import com.college.collegeconnect.databinding.FragmentHomeBinding
 import com.college.collegeconnect.datamodels.SaveSharedPreference
 import com.college.collegeconnect.viewmodels.HomeViewModel
 import com.college.collegeconnect.settingsActivity.SettingsActivity
@@ -28,6 +29,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
+import com.sample.viewbinding.fragment.viewBinding
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -35,15 +37,11 @@ import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
+    private val homeViewBinding: FragmentHomeBinding by viewBinding()
     var bottomNavigationView: BottomNavigationView? = null
     var drawable: TextDrawable? = null
     lateinit var tv: TextView
-    var totalAttendance: TextView? = null
-    var nameField: TextView? = null
-    var enrollNo: TextView? = null
-    var branch: TextView? = null
-    var prfileImage: CircleImageView? = null
     private val storage = FirebaseStorage.getInstance()
     var uri: Uri? = null
     private var storageRef: StorageReference? = null
@@ -52,11 +50,6 @@ class HomeFragment : Fragment() {
     var registered: ListenerRegistration? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        prfileImage = view.findViewById(R.id.imageView3)
-        nameField = view.findViewById(R.id.nameField)
-        enrollNo = view.findViewById(R.id.textView3)
-        branch = view.findViewById(R.id.textView4)
-        totalAttendance = view.findViewById(R.id.aggregateAttendance)
         return view
     }
 
@@ -67,19 +60,19 @@ class HomeFragment : Fragment() {
         tv.text = "HOME"
         storageRef = storage.reference
 
-        totalAttendance!!.isEnabled = false
-        nameField!!.isEnabled = false
-        enrollNo!!.isEnabled = false
-        branch!!.isEnabled = false
+        homeViewBinding.aggregateAttendance.isEnabled = false
+        homeViewBinding.nameField.isEnabled = false
+        homeViewBinding.txtEnroll.isEnabled = false
+        homeViewBinding.txtBranch.isEnabled = false
 
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        homeViewModel.returnName().observe(requireActivity(), Observer { s -> nameField!!.text = s })
-        homeViewModel.returnRoll().observe(requireActivity(), Observer { s -> enrollNo!!.text = s })
-        homeViewModel.returnBranch().observe(requireActivity(), Observer { s -> branch!!.text = s })
+        homeViewModel.returnName().observe(requireActivity(), Observer { s -> homeViewBinding.nameField.text = s })
+        homeViewModel.returnRoll().observe(requireActivity(), Observer { s -> homeViewBinding.txtEnroll.text = s })
+        homeViewModel.returnBranch().observe(requireActivity(), Observer { s -> homeViewBinding.txtBranch.text = s })
         val file = File("/data/user/0/com.college.collegeconnect/files/dp.jpeg")
         if (file.exists()) {
             uri = Uri.fromFile(file)
-            context?.applicationContext?.let { ImageHandler().getSharedInstance(it)?.load(uri)?.into(prfileImage) }
+            context?.applicationContext?.let { ImageHandler().getSharedInstance(it)?.load(uri)?.into(homeViewBinding.imgProfile) }
             Log.d("HomeFrag", "onClick: already exists")
         } else {
             storageRef!!.child("User/" + SaveSharedPreference.getUserName(activity) + "/DP.jpeg").downloadUrl.addOnSuccessListener { uri -> // Got the download URL for 'users/me/profile.png'
@@ -87,18 +80,18 @@ class HomeFragment : Fragment() {
                 download_dp()
             }.addOnFailureListener { }
         }
-        if (uri != null) Picasso.get().load(uri).into(prfileImage)
+        if (uri != null) Picasso.get().load(uri).into(homeViewBinding.imgProfile)
 
         homeViewModel.getAttended().observe(requireActivity(), Observer { atten ->
            homeViewModel.getMissed().observe(requireActivity(), Observer { miss ->
                 if (atten != null && miss != null) {
                     val percentage = atten.toFloat() / (atten.toFloat() + miss.toFloat())
                     if (percentage.isNaN())
-                        totalAttendance!!.text = "0%"
+                        homeViewBinding.aggregateAttendance.text = "0%"
                     else
-                        totalAttendance!!.text = "%.0f".format(percentage*100)+"%"
+                        homeViewBinding.aggregateAttendance.text = "%.0f".format(percentage*100)+"%"
                 } else
-                    totalAttendance!!.text = "0%"
+                    homeViewBinding.aggregateAttendance.text = "0%"
             })
 
         })
@@ -107,11 +100,11 @@ class HomeFragment : Fragment() {
                 if (atten != null && miss != null) {
                     val percentage = atten.toFloat().div((atten.toFloat() + miss.toFloat()))
                     if (percentage.isNaN())
-                        totalAttendance!!.text = "0%"
+                        homeViewBinding.aggregateAttendance.text = "0%"
                     else
-                        totalAttendance!!.text = "%.0f".format(percentage*100)+"%"
+                        homeViewBinding.aggregateAttendance.text = "%.0f".format(percentage*100)+"%"
                 } else
-                    totalAttendance!!.text = "0%"
+                    homeViewBinding.aggregateAttendance.text = "0%"
             })
         })
 
@@ -170,7 +163,7 @@ class HomeFragment : Fragment() {
                     try {
                         val fileUri = c.getString(c.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI))
                         uri = Uri.parse(fileUri)
-                        Picasso.get().load(uri).into(prfileImage)
+                        Picasso.get().load(uri).into(homeViewBinding.imgProfile)
                         copyFile("/storage/emulated/0/Android/data/" + BuildConfig.APPLICATION_ID + "/files", "/dp.jpeg", requireContext().filesDir.absolutePath)
                         File("/storage/emulated/0/Android/data/com.college.collegeconnect/files/dp.jpeg").delete()
                     } catch (e: Exception) {
@@ -232,7 +225,7 @@ class HomeFragment : Fragment() {
                 uri = Uri.fromFile(file)
                 Picasso.get().invalidate(uri)
                 SaveSharedPreference.setClearall1(context, false)
-                Picasso.get().load(uri).into(prfileImage)
+                Picasso.get().load(uri).into(homeViewBinding.imgProfile)
             }
         }
     }
